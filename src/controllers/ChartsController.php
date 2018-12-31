0694f4104dd20e975019973359b84e1503805b8c
Andrew Welch <andrew@nystudio107.com>
2018-12-30 23:48:38 -0500

    Add charts/tables controllers
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ src/controllers/ChartsController.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Webperf plugin for Craft CMS 3.x
+ *
+ * Monitor the performance of your webpages through real-world user timing data
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2018 nystudio107
+ */
+
+namespace nystudio107\webperf\controllers;
+
+use Craft;
+use nystudio107\webperf\helpers\Permission as PermissionHelper;
+
+use craft\db\Query;
+use craft\helpers\ArrayHelper;
+use craft\web\Controller;
+
+use yii\web\ForbiddenHttpException;
+use yii\web\Response;
+
+/**
+ * @author    nystudio107
+ * @package   Webperf
+ * @since     1.0.0
+ */
+class ChartsController extends Controller
+{
+    // Constants
+    // =========================================================================
+
+    // Protected Properties
+    // =========================================================================
+
+    /**
+     * @var    bool|array
+     */
+    protected $allowAnonymous = [
+    ];
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * The Dashboard chart
+     *
+     * @param string $range
+     * @param int    $siteId
+     *
+     * @return Response
+     * @throws ForbiddenHttpException
+     */
+    public function actionDashboard(string $range = 'day', int $siteId = 0): Response
+    {
+        PermissionHelper::controllerPermissionCheck('webperf:dashboard');
+        $data = [];
+        $days = 1;
+        switch ($range) {
+            case 'day':
+                $days = 1;
+                break;
+            case 'week':
+                $days = 7;
+                break;
+            case 'month':
+                $days = 30;
+                break;
+        }
+        // Different dbs do it different ways
+        $stats = null;
+        $db = Craft::$app->getDb();
+        if ($db->getIsMysql()) {
+            // Query the db
+            $query = (new Query())
+                ->from('{{%retour_stats}}')
+                ->select([
+                    "DATE_FORMAT(hitLastTime, '%Y-%m-%d') AS date_formatted",
+                    'COUNT(redirectSrcUrl) AS cnt',
+                    'COUNT(handledByRetour = 1 or null) as handled_cnt',
+                ])
+                ->where("hitLastTime >= ( CURDATE() - INTERVAL '{$days}' DAY )");
+            if ((int)$siteId !== 0) {
+                $query->andWhere(['siteId' => $siteId]);
+            }
+            $query
+                ->orderBy('date_formatted ASC')
+                ->groupBy('date_formatted');
+            $stats = $query->all();
+        }
+        if ($db->getIsPgsql()) {
+            // Query the db
+            $query = (new Query())
+                ->from('{{%retour_stats}}')
+                ->select([
+                    "to_char(\"hitLastTime\", 'yyyy-mm-dd') AS date_formatted",
+                    "COUNT(\"redirectSrcUrl\") AS cnt",
+                    "COUNT(CASE WHEN \"handledByRetour\" = true THEN 1 END) as handled_cnt",
+                ])
+                ->where("\"hitLastTime\" >= ( CURRENT_TIMESTAMP - INTERVAL '{$days} days' )");
+            if ((int)$siteId !== 0) {
+                $query->andWhere(['siteId' => $siteId]);
+            }
+            $query
+                ->orderBy('date_formatted ASC')
+                ->groupBy('date_formatted');
+            $stats = $query->all();
+        }
+        if ($stats) {
+            $data[] = [
+                'name' => '404 hits',
+                'data' => array_merge(['0'], ArrayHelper::getColumn($stats, 'cnt')),
+                'labels' => array_merge(['-'], ArrayHelper::getColumn($stats, 'date_formatted')),
+            ];
+            $data[] = [
+                'name' => 'Handled 404 hits',
+                'data' => array_merge(['0'], ArrayHelper::getColumn($stats, 'handled_cnt')),
+                'labels' => array_merge(['-'], ArrayHelper::getColumn($stats, 'date_formatted')),
+            ];
+        }
+
+        return $this->asJson($data);
+    }
+
+    /**
+     * The Dashboard chart
+     *
+     * @param int $days
+     *
+     * @return Response
+     */
+    public function actionWidget($days = 1): Response
+    {
+        $data = [];
+        // Different dbs do it different ways
+        $stats = null;
+        $handledStats = null;
+        $db = Craft::$app->getDb();
+        if ($db->getIsMysql()) {
+            // Query the db
+            $stats = (new Query())
+                ->from('{{%retour_stats}}')
+                ->where("hitLastTime >= ( CURDATE() - INTERVAL '{$days}' DAY )")
+                ->count();
+            $handledStats = (new Query())
+                ->from('{{%retour_stats}}')
+                ->where("hitLastTime >= ( CURDATE() - INTERVAL '{$days}' DAY )")
+                ->andWhere('handledByRetour is TRUE')
+                ->count();
+        }
+        if ($db->getIsPgsql()) {
+            // Query the db
+            $stats = (new Query())
+                ->from('{{%retour_stats}}')
+                ->where("\"hitLastTime\" >= ( CURRENT_TIMESTAMP - INTERVAL '{$days} days' )")
+                ->count();
+            $handledStats = (new Query())
+                ->from('{{%retour_stats}}')
+                ->where("\"hitLastTime\" >= ( CURRENT_TIMESTAMP - INTERVAL '{$days} days' )")
+                ->andWhere('"handledByRetour" = TRUE')
+                ->count();
+        }
+        if ($stats) {
+            $data = [
+                (int)$stats,
+                (int)$handledStats,
+            ];
+        }
+
+        return $this->asJson($data);
+    }
+
+    // Protected Methods
+    // =========================================================================
+}
---
+++ src/controllers/TablesController.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * Webperf plugin for Craft CMS 3.x
+ *
+ * Monitor the performance of your webpages through real-world user timing data
+ *
+ * @link      https://nystudio107.com
+ * @copyright Copyright (c) 2018 nystudio107
+ */
+
+namespace nystudio107\webperf\controllers;
+
+use nystudio107\webperf\helpers\Permission as PermissionHelper;
+
+use craft\db\Query;
+use craft\helpers\UrlHelper;
+use craft\web\Controller;
+
+use yii\web\ForbiddenHttpException;
+use yii\web\Response;
+
+/**
+ * @author    nystudio107
+ * @package   Webperf
+ * @since     1.0.0
+ */
+class TablesController extends Controller
+{
+    // Constants
+    // =========================================================================
+
+    // Protected Properties
+    // =========================================================================
+
+    /**
+     * @var    bool|array
+     */
+    protected $allowAnonymous = [
+    ];
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * Handle requests for the dashboard statistics table
+     *
+     * @param string $sort
+     * @param int    $page
+     * @param int    $per_page
+     * @param string $filter
+     * @param null   $siteId
+     *
+     * @return Response
+     * @throws ForbiddenHttpException
+     */
+    public function actionDashboard(
+        string $sort = 'hitCount|desc',
+        int $page = 1,
+        int $per_page = 20,
+        $filter = '',
+        $siteId = 0
+    ): Response {
+        PermissionHelper::controllerPermissionCheck('webperf:dashboard');
+        $data = [];
+        $sortField = 'hitCount';
+        $sortType = 'DESC';
+        // Figure out the sorting type
+        if ($sort !== '') {
+            if (strpos($sort, '|') === false) {
+                $sortField = $sort;
+            } else {
+                list($sortField, $sortType) = explode('|', $sort);
+            }
+        }
+        // Query the db table
+        $offset = ($page - 1) * $per_page;
+        $query = (new Query())
+            ->from(['{{%retour_stats}}'])
+            ->offset($offset)
+            ->limit($per_page)
+            ->orderBy("{$sortField} {$sortType}");
+        if ((int)$siteId !== 0) {
+            $query->where(['siteId' => $siteId]);
+        }
+        if ($filter !== '') {
+            $query->where(['like', 'redirectSrcUrl', $filter]);
+            $query->orWhere(['like', 'referrerUrl', $filter]);
+        }
+        $stats = $query->all();
+        if ($stats) {
+            // Add in the `addLink` field
+            foreach ($stats as &$stat) {
+                $stat['addLink'] = '';
+                if (!$stat['handledByRetour']) {
+                    $encodedUrl = urlencode('/'.ltrim($stat['redirectSrcUrl'], '/'));
+                    $stat['addLink'] = UrlHelper::cpUrl('retour/add-redirect', [
+                        'defaultUrl' => $encodedUrl
+                    ]);
+                }
+            }
+            // Format the data for the API
+            $data['data'] = $stats;
+            $query = (new Query())
+                ->from(['{{%retour_stats}}']);
+            if ($filter !== '') {
+                $query->where(['like', 'redirectSrcUrl', $filter]);
+                $query->orWhere(['like', 'referrerUrl', $filter]);
+            }
+            $count = $query->count();
+            $data['links']['pagination'] = [
+                'total' => $count,
+                'per_page' => $per_page,
+                'current_page' => $page,
+                'last_page' => ceil($count / $per_page),
+                'next_page_url' => null,
+                'prev_page_url' => null,
+                'from' => $offset + 1,
+                'to' => $offset + ($count > $per_page ? $per_page : $count),
+            ];
+        }
+
+        return $this->asJson($data);
+    }
+
+    /**
+     * Handle requests for the dashboard redirects table
+     *
+     * @param string $sort
+     * @param int    $page
+     * @param int    $per_page
+     * @param string $filter
+     * @param null   $siteId
+     *
+     * @return Response
+     * @throws ForbiddenHttpException
+     */
+    public function actionRedirects(
+        string $sort = 'hitCount|desc',
+        int $page = 1,
+        int $per_page = 20,
+        $filter = '',
+        $siteId = 0
+    ): Response {
+        PermissionHelper::controllerPermissionCheck('webperf:redirects');
+        $data = [];
+        $sortField = 'hitCount';
+        $sortType = 'DESC';
+        // Figure out the sorting type
+        if ($sort !== '') {
+            if (strpos($sort, '|') === false) {
+                $sortField = $sort;
+            } else {
+                list($sortField, $sortType) = explode('|', $sort);
+            }
+        }
+        // Query the db table
+        $offset = ($page - 1) * $per_page;
+        $query = (new Query())
+            ->from(['{{%retour_static_redirects}}'])
+            ->offset($offset)
+            ->limit($per_page)
+            ->orderBy("{$sortField} {$sortType}");
+        if ((int)$siteId !== 0) {
+            $query->where(['siteId' => $siteId]);
+        }
+        if ($filter !== '') {
+            $query->where(['like', 'redirectSrcUrl', $filter]);
+            $query->orWhere(['like', 'redirectDestUrl', $filter]);
+        }
+        $redirects = $query->all();
+        // Add in the `deleteLink` field
+        foreach ($redirects as &$redirect) {
+            $redirect['deleteLink'] = UrlHelper::cpUrl('retour/delete-redirect/'.$redirect['id']);
+            $redirect['editLink'] = UrlHelper::cpUrl('retour/edit-redirect/'.$redirect['id']);
+        }
+        // Format the data for the API
+        if ($redirects) {
+            $data['data'] = $redirects;
+            $query = (new Query())
+                ->from(['{{%retour_static_redirects}}']);
+            if ($filter !== '') {
+                $query->where(['like', 'redirectSrcUrl', $filter]);
+                $query->orWhere(['like', 'redirectDestUrl', $filter]);
+            }
+            $count = $query->count();
+            $data['links']['pagination'] = [
+                'total' => $count,
+                'per_page' => $per_page,
+                'current_page' => $page,
+                'last_page' => ceil($count / $per_page),
+                'next_page_url' => null,
+                'prev_page_url' => null,
+                'from' => $offset + 1,
+                'to' => $offset + ($count > $per_page ? $per_page : $count),
+            ];
+        }
+
+        return $this->asJson($data);
+    }
+
+    // Protected Methods
+    // =========================================================================
+}
