5d825f05bc9bfc903b7cf41dfb525e8ef7154a6a
Andrew Welch <andrew@nystudio107.com>
2019-01-09 21:14:02 -0500

    Add initial DashboardFileList component
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

---
+++ src/assetbundles/webperf/src/vue/DashboardFileList.vue
@@ -0,0 +1,118 @@
+<template>
+    <section>
+        <div class="text-left text-base font-bold">
+            Slowest pages
+        </div>
+        <div v-for="item in series" class="simple-bar-chart-wrapper p-5">
+            <div class="clearafter py-2">
+                <div class="simple-bar-chart-label text-base font-bold">{{ item.title }}</div>
+                <div class="simple-bar-chart-label text-base font-bold">{{ item.url }}</div>
+                <div class="simple-bar-chart-value text-base font-bold">{{ statFormatter(item.data) }}</div>
+            </div>
+            <div class="py-2">
+                <div class="simple-bar-chart-track rounded-full bg-grey-lighter">
+                    <div class="h-3 rounded-full" :style="{ width: item.data + '%', backgroundColor: triBlend.colorFromPercentage(item.data) }"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    import Axios from 'axios';
+    import TriBlendColor from '../js/tri-color-blend';
+
+    const chartDataBaseUrl = '/webperf/charts/dashboard-slowest-pages/';
+
+    // Configure the api endpoint
+    const configureApi = (url) => {
+        return {
+            baseURL: url,
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        };
+    };
+    // Query our API endpoint via an XHR GET
+    const queryApi = (api, uri, callback) => {
+        api.get(uri
+        ).then((result) => {
+            if (callback) {
+                callback(result.data);
+            }
+        }).catch((error) => {
+            console.log(error);
+        })
+    };
+
+    // Our component exports
+    export default {
+        components: {
+        },
+        props: {
+            range: String,
+            column: String,
+            maxValue: Number,
+            siteId: {
+                type: Number,
+                default: 0,
+            }
+        },
+        methods: {
+            // Load in our chart data asynchronously
+            getSeriesData: async function() {
+                const chartsAPI = Axios.create(configureApi(chartDataBaseUrl));
+                let uri = this.displayRange + '/' + this.column;
+                if (this.siteId !== 0) {
+                    uri += '/' + this.siteId;
+                }
+                await queryApi(chartsAPI, uri, (data) => {
+                    // Clone the chartOptions object, and replace the needed values
+                    const options = Object.assign({}, this.chartOptions);
+                    if (data[0] !== undefined) {
+                        let val = data[0] / 1000;
+                        if (val > this.displayMaxValue) {
+                            this.displayMaxValue = val;
+                        }
+                        val = (val * 100) / this.displayMaxValue;
+                        this.barColor = this.triBlend.colorFromPercentage(val);
+                        this.series = [val];
+                    }
+                });
+            },
+            onChangeRange (range) {
+                this.displayRange = range;
+                this.getSeriesData();
+            },
+            statFormatter(val) {
+                val = (val * this.displayMaxValue) / 100;
+                return Number(val).toFixed(2) + "s";
+            }
+        },
+        created () {
+            console.log(this.series);
+            //this.getSeriesData();
+        },
+        mounted() {
+            this.$events.$on('change-range', eventData => this.onChangeRange(eventData));
+            // Live refresh the data
+            setInterval(() => {
+                //this.getSeriesData();
+            }, 3000);
+        },
+        data: function() {
+            return {
+                series: [
+                    {
+                        title: 'some title',
+                        url: 'http://example.com',
+                        data: 10
+                    }
+                ],
+                displayRange: this.range,
+                displayMaxValue: this.maxValue,
+                triBlend: new TriBlendColor,
+            }
+        },
+    }
+</script>
