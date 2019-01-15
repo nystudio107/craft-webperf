829bbb548adb77412ac1f7f13aa6678824b7da8f
Andrew Welch <andrew@nystudio107.com>
2019-01-15 17:34:54 -0500

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/webperf/dist/js/vendor.js
+++ src/assetbundles/webperf/dist/js/vendor.js
@@ -2,8 +2,8 @@
  * @project        Webperf
  * @name           vendor.js
  * @author         Andrew Welch
- * @build          Mon, Jan 14, 2019 11:20 PM ET
- * @release        c3c0326a0c89a9c1252007a191aa312c3c3ed319 [develop]
+ * @build          Tue, Jan 15, 2019 10:20 PM ET
+ * @release        c21083c73f8721f6e02548104263babf772e0087 [develop]
  * @copyright      Copyright (c) 2019 nystudio107
  *
  */
