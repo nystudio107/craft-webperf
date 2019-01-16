f93a5ba518747928d0080ebae3f106ee9ab2ccb4
Andrew Welch <andrew@nystudio107.com>
2019-01-16 03:31:12 -0500

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/webperf/dist/js/styles.js
+++ src/assetbundles/webperf/dist/js/styles.js
@@ -2,8 +2,8 @@
  * @project        Webperf
  * @name           styles.js
  * @author         Andrew Welch
- * @build          Wed, Jan 16, 2019 6:05 AM ET
- * @release        d9d9af6d334269e9d85c7c648a625f788867929d [develop]
+ * @build          Wed, Jan 16, 2019 8:09 AM ET
+ * @release        280732708f4ec7e88e1ee73c30fb0e512b97e067 [develop]
  * @copyright      Copyright (c) 2019 nystudio107
  *
  */
