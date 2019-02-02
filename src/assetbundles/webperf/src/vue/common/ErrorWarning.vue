bdd1f71f890dfabee34fab14603050061a4f82b7
Andrew Welch <andrew@nystudio107.com>
2019-02-02 11:01:38 -0500

    Dark red for errors
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/webperf/src/vue/common/ErrorWarning.vue
+++ src/assetbundles/webperf/src/vue/common/ErrorWarning.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="field text-sm font-normal inline-block">
-        <p class="warning display-block" v-if="sample > 0">{{ sample }}</p>
+        <p class="warning display-block webperf-error-color" v-if="sample > 0">{{ sample }}</p>
         <p v-else class="display-block text-grey">-</p>
     </div>
 </template>
