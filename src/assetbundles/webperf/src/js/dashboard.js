74ec3a9a63c683693025e3de9f908ec210eb4d0e
Andrew Welch <andrew@nystudio107.com>
2019-06-04 23:25:48 -0400

    Add module.hot.accept()
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/webperf/src/js/dashboard.js
+++ src/assetbundles/webperf/src/js/dashboard.js
@@ -26,3 +26,8 @@ const vm = new Vue({
     mounted() {
     },
 });
+
+// Accept HMR as per: https://webpack.js.org/api/hot-module-replacement#accept
+if (module.hot) {
+    module.hot.accept();
+}
--- src/assetbundles/webperf/src/js/sidebar.js
+++ src/assetbundles/webperf/src/js/sidebar.js
@@ -16,3 +16,8 @@ const vm = new Vue({
     mounted() {
     },
 });
+
+// Accept HMR as per: https://webpack.js.org/api/hot-module-replacement#accept
+if (module.hot) {
+    module.hot.accept();
+}
