668494382adf817e86500082f4b15d897630c76c
Andrew Welch <andrew@keluli.local>
2020-12-08 16:32:38 -0500

    Updated assets build

---
+++ src/assetbundles/webperf/dist/js/webperf.js
@@ -0,0 +1,10 @@
+/*!
+ * @project        webperf
+ * @name           webperf.js
+ * @author         Andrew Welch
+ * @build          Tue Dec 08 2020 21:18:51 GMT+0000 (Coordinated Universal Time)
+ * @copyright      Copyright (c) 2020 ©2020 nystudio107.com
+ *
+ */
+(self.webpackChunkwebperf=self.webpackChunkwebperf||[]).push([[696],{6518:function(e,p,f){"use strict";f.p}},0,[[6518,666]]]);
+//# sourceMappingURL=webperf.js.map
