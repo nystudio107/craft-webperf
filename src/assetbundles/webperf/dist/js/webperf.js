0332796f9743da8b2168a4bb89eeb36f192a4cb9
Andrew Welch <andrew@nystudio107.com>
2020-12-08 23:29:57 -0500

    Updated assets build
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/assetbundles/webperf/dist/js/webperf.js
+++ src/assetbundles/webperf/dist/js/webperf.js
@@ -2,7 +2,7 @@
  * @project        webperf
  * @name           webperf.js
  * @author         Andrew Welch
- * @build          Wed Dec 09 2020 03:41:55 GMT+0000 (Coordinated Universal Time)
+ * @build          Wed Dec 09 2020 04:28:09 GMT+0000 (Coordinated Universal Time)
  * @copyright      Copyright (c) 2020 ©2020 nystudio107.com
  *
  */
