73b773fbd695f32daddfac650fbae45a13d480bf
Andrew Welch <andrew@nystudio107.com>
2021-12-22 20:08:19 -0500

    feat: Update to use Tailwind CSS `^3.0.0`

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -1,28 +1,10 @@
 // module exports
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: [
-      '../src/templates/**/*.{twig,html}',
-      '../src/assetbundles/webperf/src/vue/**/*.{vue,html}',
-      './node_modules/vuetable-2/src/components/**/*.{vue,html}',
-      './node_modules/vue-ctk-date-time-picker/dist/**/*.{vue,html}',
-    ],
-    layers: [
-      'base',
-      'components',
-      'utilities',
-    ],
-    mode: 'layers',
-    options: {
-      whitelist: [
-        '../src/assetbundles/webperf/src/css/components/**/*.{css,pcss}',
-        './node_modules/vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css',
-      ],
-    }
-  },
+  content: [
+    '../src/templates/**/*.{twig,html}',
+    '../src/assetbundles/webperf/src/vue/**/*.{vue,html,js}',
+  ],
   theme: {
   },
-  corePlugins: {},
   plugins: [],
 };
