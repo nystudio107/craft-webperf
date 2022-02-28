62e3bb6bea9f85df7e0588e26ae6d3a18284ece3
Andrew Welch <andrew@nystudio107.com>
2022-02-28 00:28:36 -0500

    chore: Updated assets build

--- src/web/assets/dist/assets/DataSampleDate.822e2357.js
+++
@@ -1,4 +0,0 @@
-import{n as a}from"./vendor.d298d7b0.js";var l=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("span",{staticClass:"cursor-default",attrs:{title:t.title}},[t._v(t._s(t.date))])},s=[];const u={name:"DataSampleDate",props:{date:{type:String,default:""},url:{type:String,default:""},query:{type:String,default:""}},computed:{title(){let t="";return t+="Url: "+this.url,this.query&&(t+=`
-
-Query: `+this.query),t}}},r={};var i=a(u,l,s,!1,o,null,null,null);function o(t){for(let e in r)this[e]=r[e]}var c=function(){return i.exports}();export{c as D};
-//# sourceMappingURL=DataSampleDate.822e2357.js.map
