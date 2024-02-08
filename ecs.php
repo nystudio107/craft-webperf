c2a527aee0e76c484380506d661ea6df5535f56e
Andrew Welch <andrew@nystudio107.com>
2024-02-08 00:56:15 -0500

    refactor: Don’t check __FILE__

--- ecs.php
+++ ecs.php
@@ -6,7 +6,7 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/src',
-        __FILE__,
     ]);
+    $ecsConfig->parallel();
     $ecsConfig->sets([SetList::CRAFT_CMS_4]);
 };
